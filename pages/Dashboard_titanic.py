77d4a730ccda75e0b32513666799e39e5b1b5851
anithareddy0512 <102585457+anithareddy0512@users.noreply.github.com>
2023-02-27 16:18:09 +0530

    Add files via upload

--- pages/Dashboard_titanic.py
+++ pages/Dashboard_titanic.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 from matplotlib import image
 import os
 
@@ -16,3 +18,16 @@ DATA_PATH = os.path.join(dir_of_interest, "data", "titanic.csv")
 
 img = image.imread(IMAGE_PATH)
 st.image(img)
+
+df = pd.read_csv(DATA_PATH)
+st.dataframe(df)
+
+classes = st.selectbox("Select the Class:", df['pclass'].unique())
+
+col1, col2 = st.columns(2)
+
+fig_1 = px.histogram(df[df['pclass'] == classes], x="fare")
+col1.plotly_chart(fig_1, use_container_width=True)
+
+fig_2 = px.box(df[df['pclass'] == classes], y="fare")
+col2.plotly_chart(fig_2, use_container_width=True)
